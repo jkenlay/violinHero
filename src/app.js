71e544256a7b1f8072b437f0e8ce9f38d1962ac0
Jack <jkenlay@hotmail.co.uk>
2017-05-27 19:41:38 +0100

    removing something

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var port = process.env.port || 1337;
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/src'));
+//app.use(express.static(__dirname + '/src'));
 
 app.get('/', function (req, res) {
     console.log('get request' + __dirname);
@@ -24,18 +24,3 @@ app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
 
-var req = http.get(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-  // Buffer the body entirely for processing as a whole.
-  var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-    var body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-    // ...and/or process the entire body here.
-  })
-});
