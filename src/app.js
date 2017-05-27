0eac507f9ce554e74d9554260f4d3d06065c1c50
Jack <jkenlay@hotmail.co.uk>
2017-05-27 19:39:55 +0100

    testing new get

--- src/app.js
+++ src/app.js
@@ -23,3 +23,19 @@ app.post('/update', function(req, res){
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
+
+var req = http.get(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+  // Buffer the body entirely for processing as a whole.
+  var bodyChunks = [];
+  res.on('data', function(chunk) {
+    // You can process streamed parts here...
+    bodyChunks.push(chunk);
+  }).on('end', function() {
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+    // ...and/or process the entire body here.
+  })
+});
