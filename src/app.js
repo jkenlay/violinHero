323f649e0f4b3522833b56d455b533f75461f5c8
Jack <jkenlay@hotmail.co.uk>
2017-05-28 14:46:22 +0100

    progressing to automated deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var port = process.env.PORT || 3000;
 var createHandler = require('github-webhook-handler');
 var handler = createHandler({ path: '/update', secret: '1234567890' });
 
+var exec = require('child_process').exec;
+
 console.log('app.js running on port: ' + port);
 
 http.createServer(function (req, res) {
@@ -19,7 +21,13 @@ handler.on('error', function (err) {
 handler.on('push', function (event) {
 //   console.log('Received a push event for %s to %s',event.payload.repository.name,event.payload.ref)
     if(event.payload.ref=='refs/heads/master'){
-        console.log('master was pushed');
+        console.log('master was pushed, attempting to pull');
+        var cmd = 'sudo git pull';
+
+        exec(cmd, function(error, stdout, stderr) {
+        // command output is in stdout
+        console.log('from console: ' + stdout);
+        });
     }
 })
 
