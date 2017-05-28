cf6718a55c25e561a6a5ac9532089667e20253b7
Jack <jkenlay@hotmail.co.uk>
2017-05-28 14:50:58 +0100

    progressing to automated deploy

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var exec = require('child_process').exec;
 
 console.log('app.js running on port: ' + port);
 
-console.log('it should have changed now');
+console.log('it should have changed now..');
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
