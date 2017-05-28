07c7c8bb237ba0287e4d19f503f142fd8a654b53
Jack <jkenlay@hotmail.co.uk>
2017-05-28 20:13:01 +0100

    trying js contact form

--- src/js/websitejs.js
+++ src/js/websitejs.js
@@ -2,4 +2,13 @@
 
 console.log('website js loaded');
 
-//testing
+function sendContactEmail(){
+    console.log('sending email')
+    $.ajax({
+        url: "/contactformsubmit",
+        cache: false,
+        success: function(response) {
+            console.log(JSON.stringify(response));
+        }
+});
+}
