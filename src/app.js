949b9d29b03d07ea7eaf2a982db4c1e9a423613e
Jack <jkenlay@hotmail.co.uk>
2017-05-28 17:39:22 +0100

    adding src js and images

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.post('/update', function (req, res) {
     if (req.body.ref=='refs/heads/master') {//later on, will need to use sha1 and compare to header sent by gh
         //for now it pulls and uglifies, in the future may need to migrate images etc....
-        exec('sudo git pull && uglifyjs src/violinHero.js -o dist/js/violinhero.js && cp src/index.html dist/index.html',execCallBack);
+        exec('sudo git pull && uglifyjs src/violinHero.js -o dist/js/violinhero.js && cp src/index.html dist/index.html && cp -a src/images/. dist/images/ && cp -a src/js/. dist/js/ ',execCallBack);
         res.json({
             message: 'ok got it!'
         });   
