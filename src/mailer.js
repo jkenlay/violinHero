1c195ead8bfe65a5e3e9a5cda7a5169c54e7941f
Jack <jkenlay@hotmail.co.uk>
2017-05-28 19:32:05 +0100

    adding mailer doc

--- src/mailer.js
+++ src/mailer.js
@@ -1,5 +1,5 @@
 /* eslint no-console: 0 */
-
+/*
 'use strict';
 const nodemailer = require('nodemailer');
 
@@ -16,9 +16,22 @@ let transporter = nodemailer.createTransport({
         accessToken: 'ya29.GltYBJ4_Q4SL0MkEVP3NfE34Xmc28QK3ViQwc-X99TEq8ZuHBWVqxFi8oLfaJqRBbW4DW9dKV44ncPS209JYkb9M1pA2rV6mLpk_szCPYjVz6YCU9YQ7SJYsbuya',
         expires: 12345
     }
-});
+});*/
 
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        xoauth2: xoauth2.createXOAuth2Generator({
+            user: 'jackkenlay@gmail.com',
+            clientId: '218463359700-4ct8hduunfj1dusnslh9gsesqk936k44.apps.googleusercontent.com',
+            clientSecret: 'WrC3rWZhLbyKhcpkKBxc6dhn',
+            refreshToken: '1/VJVvKRYio_WkKp3Lci8w8zlAW2ljjhVrG30KLiEUNIM'
+        })
+    }
+})
 
 var message = {
     from: 'My Name <jackkenlay@gmail.com>',
