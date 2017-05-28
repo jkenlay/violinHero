b955e6a8c77c8b8e90e44f1d4695d132af63e080
Jack <jkenlay@hotmail.co.uk>
2017-05-28 15:25:48 +0100

    progressing to automated deploy

--- src/app.js
+++ src/app.js
@@ -19,6 +19,14 @@ console.log('restarting');
 //   });
 // }).listen(port);
 
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
 handler.on('error', function (err) {
   console.error('Error:', err.message)
 });
