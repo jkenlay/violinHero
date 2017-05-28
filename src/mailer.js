73ba2d23667797ac194eb4f0f0229e19ecca52ef
Jack <jkenlay@hotmail.co.uk>
2017-05-28 19:35:47 +0100

    try again

--- src/mailer.js
+++ src/mailer.js
@@ -21,17 +21,29 @@ let transporter = nodemailer.createTransport({
 const nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        xoauth2: xoauth2.createXOAuth2Generator({
-            user: 'jackkenlay@gmail.com',
-            clientId: '218463359700-4ct8hduunfj1dusnslh9gsesqk936k44.apps.googleusercontent.com',
-            clientSecret: 'WrC3rWZhLbyKhcpkKBxc6dhn',
-            refreshToken: '1/VJVvKRYio_WkKp3Lci8w8zlAW2ljjhVrG30KLiEUNIM'
-        })
-    }
-})
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         xoauth2: xoauth2.createXOAuth2Generator({
+//             user: 'jackkenlay@gmail.com',
+//             clientId: '218463359700-4ct8hduunfj1dusnslh9gsesqk936k44.apps.googleusercontent.com',
+//             clientSecret: 'WrC3rWZhLbyKhcpkKBxc6dhn',
+//             refreshToken: '1/VJVvKRYio_WkKp3Lci8w8zlAW2ljjhVrG30KLiEUNIM'
+//         })
+//     }
+// })
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+        type: 'OAuth2',
+        user: 'jackkenlay@gmail.com',
+        clientId: '218463359700-4ct8hduunfj1dusnslh9gsesqk936k44.apps.googleusercontent.com',
+        clientSecret: 'WrC3rWZhLbyKhcpkKBxc6dhn',
+        refreshToken: '1/VJVvKRYio_WkKp3Lci8w8zlAW2ljjhVrG30KLiEUNIM',
+        accessToken: 'ya29.GltYBJ4_Q4SL0MkEVP3NfE34Xmc28QK3ViQwc-X99TEq8ZuHBWVqxFi8oLfaJqRBbW4DW9dKV44ncPS209JYkb9M1pA2rV6mLpk_szCPYjVz6YCU9YQ7SJYsbuya',
+  },
+});
 
 var message = {
     from: 'Jack Kenlay <jackkenlay@gmail.com>',
