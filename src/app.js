0124cbc062827c38799f82e14506375f159f3337
Jack <jkenlay@hotmail.co.uk>
2017-05-28 17:47:48 +0100

    fixing migrations

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 
 app.post('/update', function (req, res) {
     if (req.body.ref=='refs/heads/master') {//later on, will need to use sha1 and compare to header sent by gh
-        //for now it pulls and uglifies, in the future may need to migrate images etc..
-        exec('sudo git pull && sleep 3s && uglifyjs src/js/violinHero.js -o dist/js/violinhero.js && uglifyjs src/js/pitchdetect2.js -o dist/js/pitchdetect2.js&& cp src/index.html dist/index.html && cp -a src/images/. dist/images/ && cp -a src/js/. dist/js/ ',execCallBack);
+        //for now it pulls and uglifies, in the future may need to migrate css
+        exec('sudo git pull && sleep 3s && uglifyjs src/js/violinhero.js -o dist/js/violinhero.js && uglifyjs src/js/pitchdetect2.js -o dist/js/pitchdetect2.js&& cp src/index.html dist/index.html && cp -a src/images/. dist/images/ && echo "fininshed scripts"',execCallBack);
         res.json({
             message: 'ok got it!'
         });   
