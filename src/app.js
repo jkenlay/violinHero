1f9923667003fcb0aff22dbb333bfc1189f1e532
jkenlay <jkenlay@hotmail.co.uk>
2017-08-05 20:18:13 +0100

    adds dist

--- src/app.js
+++ src/app.js
@@ -10,31 +10,6 @@ var emailer = require('./mailer.js');
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 
-
-// a = (2)1/12
-
-// let a = 196 * Math.pow(2,1/12); //1 note up
-// let b = 196 * Math.pow(2,2/12); // 2 notes up
-// let c = 196 * Math.pow(2,3/12); // 3 notes up
-//next G up is 392
-//GS = a 207.65 =   k 196 * b 2^(c 1/12)
-
-// c = log(b)*a
-
-
-
-function isNoteAMatch(inputfreq, currentFreq){ 
-    //let testA = Math.log(2*(1/12));
-    let testA = 12*(Math.log2(inputfreq/currentFreq));
-    console.log('Test: '+testA);
-}
-isNoteAMatch(392,196);
-
-
-
-let G = 196;
-let G2 = 196*Math.pow(2,1/12);
-
 app.post('/update', function (req, res) {
     if (req.body.ref=='refs/heads/master') {//later on, will need to use sha1 and compare to header sent by gh
         //for now it pulls and uglifies, in the future may need to uglify css?
